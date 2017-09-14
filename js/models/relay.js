6a45da633086120a6b8083404f3aefb7dc585b45
Iain R. Learmonth <irl@fsfe.org>
2017-09-14 11:37:51 +0100

    Adds DNS names to relay details pages

--- js/models/relay.js
+++ js/models/relay.js
@@ -167,6 +167,7 @@ define([
                     relay.or_address = relay.or_addresses ? relay.or_addresses[0].split(":")[0] : null;
                     relay.or_port = relay.or_addresses ? relay.or_addresses[0].split(":")[1] : 0;
                     relay.dir_port = relay.dir_address ? relay.dir_address.split(":")[1] : 0;
+                    relay.host_name = relay.host_name ? relay.host_name : null;
                     relay.country = relay.country ? relay.country.toLowerCase() : null;
                     relay.countryname = relay.country ? CountryCodes[relay.country] : null;
                     relay.first_seen = relay.first_seen ? relay.first_seen : null;
